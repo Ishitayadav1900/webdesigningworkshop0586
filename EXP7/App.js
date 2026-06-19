03aa5ddf1cfacf7f7db9587b05572964cf4d11fe
Ishita Yadav <yadavishita63@gmail.com>
2026-06-19 21:56:08 +0530

    done

---
+++ EXP7/App.js
@@ -0,0 +1,8 @@
+function App() {
+    return (
+        <div className="App">
+            <h1>Welcome to React</h1>
+            <p style={{ color: 'blue' }}>This is a simple React application.</p>
+        </div>
+    );
+}
