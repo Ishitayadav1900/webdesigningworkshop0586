1b5db0ac3596ca86a79f7217933fdc446edd6b43
Ishita Yadav <yadavishita63@gmail.com>
2026-06-19 22:03:01 +0530

    done

--- empolyee.java
+++ empolyee.java
@@ -44,7 +44,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType; 
 import jakarta.persistence.Id; 
 @Entity 
-public class Employee { 
+public class  Employee { 
 @Id 
 @GeneratedValue(strategy = GenerationType.IDENTITY) 
 private Long id; 
